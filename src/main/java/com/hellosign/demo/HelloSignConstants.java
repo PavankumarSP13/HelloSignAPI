9578e3646586995199bc9cf6a60c930a2506e500
TRICONHOME\usha.reddyr <usha.reddyr@triconinfotech.com>
2022-07-27 18:16:43 +0530

    Stamping data with send template ID

--- src/main/java/com/hellosign/demo/HelloSignConstants.java
+++ src/main/java/com/hellosign/demo/HelloSignConstants.java
@@ -5,4 +5,23 @@ public class HelloSignConstants {
     public static final String STUDENT_CLERKSHIP_EVALUATION_TEMPLATE_NAME = "Student Clerkship Evaluation";
     public static final String STUDENT_CLERKSHIP_EVALUATION_TEMPLATE_PATH = "StudentClerkshipEvaluationForm.pdf";
 
+    public static final int MID_CLERKSHIP_EVALUATION_FORM_ID  =3;
+    public static final String MID_CLERKSHIP_EVALUATION_TEMPLATE_NAME = "Mid-Clerkship Assessment";
+    public static final String MID_CLERKSHIP_EVALUATION_TEMPLATE_PATH = "MidClerkshipAssessmentForm.pdf";
+
+    public static final int STUDENT_FACULTY_EVALUATION_FORM_ID  =4;
+    public static final String STUDENT_FACULTY_EVALUATION_TEMPLATE_NAME = "Student Faculty Evaluation";
+    public static final String STUDENT_FACULTY_EVALUATION_TEMPLATE_PATH = "StudentFacultyEvaluationForm.pdf";
+
+    public static final int AUA_CORE_OSCE_FORM_ID  =6;
+    public static final String AUA_CORE_OSCE_TEMPLATE_NAME = "Core Osce Form";
+    public static final String AUA_CORE_OSCE_TEMPLATE_PATH = "AUA_Formative_OSCE.pdf";
+
+    public static final String STUDENT_PORTFOLIO_TEMPLATE_NAME = "Student Portfolio";
+    public static final String STUDENT_PORTFOLIO_TEMPLATE_PATH = "StudentPortfolioForm.pdf";
+    public static final int STUDENT_PORTFOLIO_FORM_ID  =5;
+
+    public static final String COMPREHENSIVE_CLERKSHIP_TEMPLATE_NAME = "Comprehensive Clerkship Evaluation";
+    public static final String COMPREHENSIVE_CLERKSHIP_TEMPLATE_PATH = "ComprehensiveStudentClerkshipAssessmentForm.pdf";
+    public static final int COMPREHENSIVE_CLERKSHIP_FORM_ID  =2;
 }
